ed377e77bda68b33b9842bb822f5356360a2e6b7
Joonas Westlin <joonas.westlin@gmail.com>
2018-01-27 20:28:05 +0200

    Some clean-up.

--- ApiOnBehalfSample/Services/GraphApiService.cs
+++ ApiOnBehalfSample/Services/GraphApiService.cs
@@ -1,33 +1,15 @@
-﻿using Microsoft.AspNetCore.DataProtection;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Caching.Distributed;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.Graph;
+﻿using Microsoft.Graph;
 using System.Threading.Tasks;
 
 namespace ApiOnBehalfSample.Services
 {
     public class GraphApiService : IGraphApiService
     {
-        private readonly IDistributedCache _distributedCache;
-        private readonly ILoggerFactory _loggerFactory;
-        private readonly IDataProtectionProvider _dataProtectionProvider;
         private readonly IAuthenticationProvider _msGraphAuthenticationProvider;
-        private readonly Options.AuthenticationOptions _authSettings;
 
-        public GraphApiService(
-            IOptions<Options.AuthenticationOptions> authOptions,
-            IDistributedCache distributedCache,
-            ILoggerFactory loggerFactory,
-            IDataProtectionProvider dataProtectionProvider,
-            IAuthenticationProvider authenticationProvider)
+        public GraphApiService(IAuthenticationProvider authenticationProvider)
         {
-            _distributedCache = distributedCache;
-            _loggerFactory = loggerFactory;
-            _dataProtectionProvider = dataProtectionProvider;
             _msGraphAuthenticationProvider = authenticationProvider;
-            _authSettings = authOptions.Value;
         }
 
         public async Task<User> GetUserProfileAsync()
